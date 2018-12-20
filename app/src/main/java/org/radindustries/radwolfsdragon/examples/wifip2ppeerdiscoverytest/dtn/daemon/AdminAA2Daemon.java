3096a97ab463b0955d5611bbb324934fba06d5fa
William Kibirango <williamkaos.kibirango76@gmail.com>
2018-12-20 20:42:39 +0300

    Added a common interface for AAs
      * Since both AAs request bundle transmission from the Daemon, it makes sense
        for the Daemon to just implement the common interface instead of doing it
        uniquely for each.

--- app/src/main/java/org/radindustries/radwolfsdragon/examples/wifip2ppeerdiscoverytest/dtn/daemon/AdminAA2Daemon.java
+++ app/src/main/java/org/radindustries/radwolfsdragon/examples/wifip2ppeerdiscoverytest/dtn/daemon/AdminAA2Daemon.java
@@ -1,10 +1,8 @@
 package org.radindustries.radwolfsdragon.examples.wifip2ppeerdiscoverytest.dtn.daemon;
 
-import org.radindustries.radwolfsdragon.examples.wifip2ppeerdiscoverytest.dtn.dto.DTNBundle;
 import org.radindustries.radwolfsdragon.examples.wifip2ppeerdiscoverytest.dtn.dto.DTNBundleID;
 
-public interface AdminAA2Daemon extends EIDProvider {
-    void transmit(DTNBundle adminRecord);
+public interface AdminAA2Daemon extends EIDProvider, AA2Daemon {
     void notifyOutboundBundleDelivered(String recipient);
     void delete(DTNBundleID bundleID);
     void delete(DTNBundleID bundleID, int fragmentOffset);
--- app/src/main/java/org/radindustries/radwolfsdragon/examples/wifip2ppeerdiscoverytest/dtn/daemon/AppAA2Daemon.java
+++ app/src/main/java/org/radindustries/radwolfsdragon/examples/wifip2ppeerdiscoverytest/dtn/daemon/AppAA2Daemon.java
@@ -1,7 +1,4 @@
 package org.radindustries.radwolfsdragon.examples.wifip2ppeerdiscoverytest.dtn.daemon;
 
-import org.radindustries.radwolfsdragon.examples.wifip2ppeerdiscoverytest.dtn.dto.DTNBundle;
-
-public interface AppAA2Daemon extends EIDProvider {
-    void transmit(DTNBundle bundle);
+public interface AppAA2Daemon extends EIDProvider, AA2Daemon {
 }
